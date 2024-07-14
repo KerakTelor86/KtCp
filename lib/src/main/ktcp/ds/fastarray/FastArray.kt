00e366d213d257e6ca0e2067ccedd6dd88014ae3
Richard Alison <richardalison86@gmail.com>
2024-07-14 15:14:08 +0900

    feat(ds-fastarray): add FastList

--- lib/src/main/ktcp/ds/fastarray/FastArray.kt
+++ lib/src/main/ktcp/ds/fastarray/FastArray.kt
@@ -13,6 +13,20 @@ value class FastArray @PublishedApi internal constructor(
     companion object;
 }
 
+class FastList @PublishedApi internal constructor(
+    @PublishedApi internal var buffer: ByteBuffer,
+) {
+    companion object;
+
+    val size: Int
+        get() = buffer.getInt(0)
+
+    @PublishedApi
+    internal inline fun setSize(value: Int) {
+        buffer.putInt(0, value)
+    }
+}
+
 @JvmInline
 value class FastArrayContext<T>(
     @PublishedApi internal val serializer: ByteSerializer<T>,
@@ -45,8 +59,65 @@ value class FastArrayContext<T>(
         val res = transform(serializer.deserialize(buffer, bufIndex))
         serializer.serialize(buffer, bufIndex, res)
     }
+
+    inline val FastList.capacity
+        get() = (buffer.capacity() - 4) / serializer.bytesRequired
+
+    operator fun FastList.Companion.invoke(size: Int): FastList =
+        FastList(
+            ByteBuffer.allocate(4 + size * serializer.bytesRequired)
+        ).also {
+            it.setSize(size)
+        }
+
+    inline operator fun FastList.Companion.invoke(
+        size: Int,
+        init: (Int) -> T,
+    ): FastList {
+        val arr = FastList(size)
+        for (i in 0..<size) {
+            arr[i] = init(i)
+        }
+        return arr
+    }
+
+    inline operator fun FastList.get(index: Int): T =
+        serializer.deserialize(buffer, 4 + index * serializer.bytesRequired)
+
+    inline operator fun FastList.set(index: Int, value: T) =
+        serializer.serialize(
+            buffer,
+            4 + index * serializer.bytesRequired,
+            value
+        )
+
+    inline fun FastList.transform(index: Int, transform: (T) -> T) {
+        val bufIndex = 4 + index * serializer.bytesRequired
+        val res = transform(serializer.deserialize(buffer, bufIndex))
+        serializer.serialize(buffer, bufIndex, res)
+    }
+
+    inline fun FastList.pop() = setSize(size - 1)
+
+    inline fun FastList.add(value: T) {
+        if (size == capacity) {
+            buffer = ByteBuffer
+                .allocate(
+                    4 + capacity * serializer.bytesRequired * 2
+                )
+                .rewind()
+                .put(buffer.rewind())
+        }
+        this[size] = value
+        setSize(size + 1)
+    }
+
+    inline fun FastList.addAll(elements: Collection<T>) = elements.forEach {
+        add(it)
+    }
 }
 
 // exports: FastArray
+// exports: FastList
 // exports: FastArrayContext
 // depends: ds/fastarray/serializer/Serializer.kt
